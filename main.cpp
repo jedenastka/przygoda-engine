aafe8eff7fecb69df9dad2f8d04593f1e0ab11fc
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2018-12-30 15:10:32 +0000

    Structs to classes. Even more compilation errors.

--- main.cpp
+++ main.cpp
@@ -7,13 +7,11 @@ class Game {
         Game();
         void start();
     private:
-        struct Location;
-        struct Player {
-            std::string name;
-            Location *location;
-        };
-        struct Object;
-        struct Exit;
+        
+        class Location;
+        class Player;
+        class Object;
+        class Exit;
         enum class DIRECTION;
         std::vector<Location> locations;
         Player player;
@@ -35,33 +33,47 @@ Game::Game() {
 void Game::start() {
     while (1) {
         player.location->describe();
-        takeInput();
+        //takeInput();
     }
 }
 
-struct Game::Location {
-    std::string name;
-    std::string description;
-    std::vector<Object> objects;
-    std::vector<Exit> exits;
-    void describe();
+class Game::Location {
+    public:
+        std::string name;
+        std::string description;
+        std::vector<Object> objects;
+        std::vector<Exit> exits;
+        void describe();
 };
 
 void Game::Location::describe() {
     std::cout << "You are in " << name << ".\n" << description << "\n" << "You can see ";
-    
+    for (auto i : objects) {
+        std::cout << i.name << " ";
+    }
+    std::cout << ".\n" << "You can go ";
+    for (auto i : objects) {
+        std::cout << i.direction << " ";
+    }
+    std::cout << ".\n";
 }
 
-// Here was Player
-
-struct Game::Object {
-    std::string name;
-    std::string description;
+class Game::Player {
+    public:
+        std::string name;
+        Location *location;
 };
 
-struct Game::Exit {
-    DIRECTION direction;
-    Location *location;
+class Game::Object {
+    public:
+        std::string name;
+        std::string description;
+};
+
+class Game::Exit {
+    public:
+        DIRECTION direction;
+        Location *location;
 };
 
 enum class Game::DIRECTION {N, E, S, W};
