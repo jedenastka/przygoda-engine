2e3499c4962a234f421afac5c23c8af6560a7b4b
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2019-11-27 21:02:16 +0100

    Added some class functionality.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,55 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <string>
+
+enum Direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+    UP,
+    DOWN
+};
 
 class Room {
     public:
+        Room(std::map<Direction, Room*> exitsTmp) {
+            exits[NORTH] = NULL;
+            exits[EAST] = NULL;
+            exits[SOUTH] = NULL;
+            exits[WEST] = NULL;
+            exits[UP] = NULL;
+            exits[DOWN] = NULL;
+        }
     private:
+        std::map<Direction, Room*> exits;
 };
 
 class Location {
     public:
+        Location* getRoom(std::string id);
     private:
-        std::vector<Room> rooms;
+        std::map<std::string, Room> rooms;
 };
 
 class Map {
     public:
+        Location* getLocation(std::string id);
     private:
-        std::vector<Location> locations;
+        std::map<std::string, Location*> locations;
 };
 
 class Player {
     public:
+        void go(Direction);
     private:
-        int room;
+        Room* currentRoom;
 };
 
+Player player;
+Map map;
+
 int main() {
-    Player player;
+    
 }
